2d9dd26a73e700b79edbda12b68b91a008802db2
Max Lawton <max@lawtonnation.com>
2023-10-13 09:48:17 -0400

    sql working!

--- extraction.py
+++ extraction.py
@@ -7,10 +7,6 @@ cur = conn.cursor()
 # get the names of the tables in the database
 cur.execute(" select name from sqlite_master where type = 'table' ") 
 
-SQL_BASE = """
-SELECT * FROM message
-"""
-
 SQL = """
 SELECT datetime(message.date/1000000000 + strftime("%s", "2001-01-01") ,"unixepoch","localtime") AS date_uct, id, text, is_from_me
 FROM message
@@ -18,8 +14,6 @@ LEFT JOIN handle
 ON message.handle_id = handle.ROWID
 WHERE id='+16176826385' AND text LIKE '%stella%' 
 """
-
-#16176826385
 
 # get messages using the sql
 messages = pd.read_sql_query(SQL, conn)
