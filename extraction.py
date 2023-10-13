bbd614df02189a0c88dc78066a2a67d0cb5aec4d
Max Lawton <max@lawtonnation.com>
2023-10-13 09:44:27 -0400

    rename

--- extraction.py
+++ extraction.py
@@ -7,6 +7,10 @@ cur = conn.cursor()
 # get the names of the tables in the database
 cur.execute(" select name from sqlite_master where type = 'table' ") 
 
+SQL_BASE = """
+SELECT * FROM message
+"""
+
 SQL = """
 SELECT datetime(message.date/1000000000 + strftime("%s", "2001-01-01") ,"unixepoch","localtime") AS date_uct, id, text, is_from_me
 FROM message
@@ -22,5 +26,6 @@ messages = pd.read_sql_query(SQL, conn)
 
 print(messages)
 
+#print(messages.columns.tolist())
 # sends to csv file on desktop
 #messages.iloc[244:600].to_csv('test.csv', index=False, header=False)
