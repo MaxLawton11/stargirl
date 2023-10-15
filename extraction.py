77b9e95351d9c7172c63a1d97bb23526b7b7085a
Max Lawton <max@lawtonnation.com>
2023-10-14 22:17:25 -0400

    new config

--- extraction.py
+++ extraction.py
@@ -1,5 +1,8 @@
 import sqlite3
 import pandas as pd
+import time
+import datetime
+
 # substitute username with your username
 conn = sqlite3.connect('chat.db')
 # connect to the database
@@ -20,12 +23,24 @@ WHERE id='+16176826385'
 # get messages using the sql
 messages = pd.read_sql_query(SQL, conn)
 
-messages = messages.drop(index=[i for i in range(len(messages)-10)])
+print(messages)
 
-for message in zip(messages['text'], messages['is_from_me']) :
-    if message[1] == True : # this works beacues 1==True
+messages = messages.drop(index=[i for i in range(len(messages)-100)])
+
+last_message=0
+for message, is_from_me, date_uct  in zip(messages['text'], messages['is_from_me'], messages['date_uct']) :
+    """
+    difference = int(date_uct)-int(last_message)
+    if difference > 3600 :
+        print('-'*10, "NEW CHAT", '-'*10)
+    last_message = date_uct"""
+    
+    print(date_uct)
+    print( (str(date_uct[12:]).split(':'))[0]*60 )
+    
+    """if is_from_me == True : # this works beacues 1==True
         print(" "*20, end='')
-    print(message[0])
+    print(message)"""
 
 
 
