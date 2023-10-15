37fbca8a7b8f2e4f06ec302ca0b843c2aead0163
Max Lawton <max@lawtonnation.com>
2023-10-14 22:45:02 -0400

    new chat funationality

--- extraction.py
+++ extraction.py
@@ -11,36 +11,34 @@ cur = conn.cursor()
 cur.execute(" select name from sqlite_master where type = 'table' ") 
 
 SQL = """
-SELECT datetime(message.date/1000000000 + strftime("%s", "2001-01-01") ,"unixepoch","localtime") AS date_uct, id, text, is_from_me
+SELECT datetime(message.date/1000000000 + strftime("%s", "2001-01-01"), "unixepoch", "localtime") AS date_uct, id, text, is_from_me
 FROM message
 LEFT JOIN handle
 ON message.handle_id = handle.ROWID
 WHERE id='+16176826385'
 """
 
-# WHERE id='+16176826385' AND text LIKE '%stella%' 
+# WHERE id='+16176826385' AND text LIKE '%hi%' 
 
 # get messages using the sql
 messages = pd.read_sql_query(SQL, conn)
-
-print(messages)
 
 messages = messages.drop(index=[i for i in range(len(messages)-100)])
 
 last_message=0
 for message, is_from_me, date_uct  in zip(messages['text'], messages['is_from_me'], messages['date_uct']) :
-    """
-    difference = int(date_uct)-int(last_message)
-    if difference > 3600 :
+    
+    # check if it's a new chat
+    processed_time = int(date_uct[11:].split(':')[:2][0])*60 + int(date_uct[11:].split(':')[:2][1])
+    difference = processed_time-last_message
+    if difference > 60 : # in minutes
         print('-'*10, "NEW CHAT", '-'*10)
-    last_message = date_uct"""
-    
-    print(date_uct)
-    print( (str(date_uct[12:]).split(':'))[0]*60 )
-    
-    """if is_from_me == True : # this works beacues 1==True
+    last_message = processed_time
+
+    # print message
+    if is_from_me == True : # this works beacues 1==True
         print(" "*20, end='')
-    print(message)"""
+    print(message)
 
 
 
