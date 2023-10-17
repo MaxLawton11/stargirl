3b6dcd9e0e90148af8fbc8ff7ce53a83faebc7b2
Max Lawton <max@lawtonnation.com>
2023-10-17 17:04:49 -0400

    new aproch

--- extraction.py
+++ extraction.py
@@ -22,25 +22,8 @@ messages = pd.read_sql_query(SQL, conn)
 # limit to last few
 messages = messages.drop(index=[i for i in range(len(messages)-100)])
 
-questions, answers = [], []
-#last_message=0
-for message, is_from_me, date_uct  in zip(messages['text'], messages['is_from_me'], messages['date_uct']) :
-    
-    """ This will only be used for in-dept training.
-    # check if it's a new chat
-    processed_time = int(date_uct[11:].split(':')[:2][0])*60 + int(date_uct[11:].split(':')[:2][1])
-    difference = processed_time-last_message
-    if difference > 60 : # in minutes
-        print('-'*10, "NEW CHAT", '-'*10)
-    last_message = processed_time
-    """
+print(messages)
 
-    # print message
-    if is_from_me == True : # this works beacues 0==False and 1==Ture
-        questions.append(message)
-    else :
-        answers.append(message)
-        
 """
 make one list
 combine same "thoughts" as one elsmet
@@ -49,9 +32,30 @@ use each elemnt as both qwaetion and asswer
 look up if you can add more data like text to this model
 """
 
-        
+"""
+questions, answers = [], []
+#last_message=0
+for message, is_from_me, date_uct  in zip(messages['text'], messages['is_from_me'], messages['date_uct']) :
+    
+    This will only be used for in-dept training.
+    # check if it's a new chat
+    processed_time = int(date_uct[11:].split(':')[:2][0])*60 + int(date_uct[11:].split(':')[:2][1])
+    difference = processed_time-last_message
+    if difference > 60 : # in minutes
+        print('-'*10, "NEW CHAT", '-'*10)
+    last_message = processed_time
+    
+
+    # print message
+    if is_from_me == True : # this works beacues 0==False and 1==Ture
+        questions.append(message)
+    else :
+        answers.append(message)
+"""
+
+"""
 print(questions, len(questions))
 print(answers, len(answers))
 export_dataframe = pd.DataFrame(data={'QUESTIONS':questions, 'ANSWERS':answers })
 print(export_dataframe)
-    
+"""
