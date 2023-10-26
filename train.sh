cd1721f62cf93924d2e0cbb4c7b3accadae30661
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-10-25 21:03:11 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-if test -f /instances/tokenizer.pkl; then
+
+if test -f instances/tokenizer.pkl; then
   echo "No model found. Creating new model..."
   python3 model/create.py
+else
+  echo "Model found. Skipping creation."
 fi
+
+echo "Training the model..."
 python3 model/train.py
