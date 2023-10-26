ed5f7e120aefd3f8baef0e0e687a02eb3c5100ff
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-10-25 21:26:16 -0400

    Update train.py

--- model/train.py
+++ model/train.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.metrics import SparseCategoricalAccuracy 
 import numpy as np
 import pandas as pd
 import pickle
