dc4a106426f33769fc9a98ae22ff3bd27bef7bc3
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-10-25 21:08:37 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# List all files in the 'instances' directory for debugging
-echo "Files in 'instances' directory:"
-ls instances
-
-# Check for the specific file 'tokenizer.pkl'
-if [[ -f instances/tokenizer.pkl ]]; then
-  echo "Found tokenizer.pkl. Creating new model..."
+if instances/tokenizer.pkl; then
+  echo "\$ Didn't find 'tokenizer.pkl'. Creating new model..."
   python3 model/create.py
-else
-  echo "No tokenizer.pkl found. Skipping model creation."
 fi
 
-echo "Training the model..."
+echo "\$ Training the model..."
 python3 model/train.py
