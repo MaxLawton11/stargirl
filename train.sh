de0604ad6eeb3ed98191645dd60e4417026d352a
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-10-25 21:01:06 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 if test -f instances/tokenizer.pkl; then
-  if test -f instances/model.keras; then
-    echo "No model found. Creating new model..."
-    python3 model/create.py
-  fi
+  echo "No model found. Creating new model..."
+  python3 model/create.py
 fi
 python3 model/train.py
