58feefbef42073eacd50295c820e97cfc3429638
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-10-30 09:55:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 print("\$ Installing dependencies...")
+pip3 install sqlite3
+pip3 install pandas
+pip3 install numpy
+pip3 install tensorflow
+pip3 install pickle
 
 print("\$ Setting up dataset...")
 python3 data/extraction.py
