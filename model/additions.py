98dc9dc3cd45a9b6842370dfb26c0bb7538c6690
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-11-02 21:42:04 -0400

    Update additions.py

--- model/additions.py
+++ model/additions.py
@@ -1,1 +1,9 @@
+import os.path
 
+def loadLog(file_location:str) :
+  # gate claues to see if file is real
+  if !os.path.isfile(file_location) :
+    raise Exception("Log file not found.")
+    
+  log = json.load(open(file_location))
+  return log['max_sequence_length'], log['batch_size']
