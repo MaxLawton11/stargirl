ed4ffeabb209ff4ca71b5b12b8c35960e950eefe
Max Lawton <max@lawtonnation.com>
2023-11-28 21:20:18 -0500

    okay new log format

--- working_scrips/test_trainer.py
+++ working_scrips/test_trainer.py
@@ -1,6 +1,7 @@
 import json
 
 log_filename = 'working_scrips/log.json'
+run_nepochs = 1
 
 # convert into JSON:
 log = json.load(open(log_filename)) # cuz this is not root we need teh folder idfk
@@ -8,10 +9,12 @@ log = json.load(open(log_filename)) # cuz this is not root we need teh folder id
 for c in log :
     print(f"{c} - {log[c]}")
 
+# print new files
 with open(log_filename, 'w') as log_output_file:
     new_log_contents = {
-        "epochs": 5, 
-        "max_sequence_length": 20,
-        "batch_size": 64
+        "model_filename" : log['model_filename'],
+        "epochs": log['epochs'], 
+        "max_sequence_length": log['max_sequence_length'],
+        "batch_size": log['batch_size']
         }
     json.dump(new_log_contents, log_output_file, indent=2) # save file
